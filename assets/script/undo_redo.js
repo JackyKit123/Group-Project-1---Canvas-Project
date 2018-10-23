3ab52728f455340e9f2444a6ac6ccba48f6b5bf0
Jackykit123 <jackykit19970402@gmail.com>
2018-10-23 21:11:13 +0800

    new code for pencil to reduce heaviness of undo_redo function

--- assets/script/undo_redo.js
+++ assets/script/undo_redo.js
@@ -3,36 +3,20 @@ let undoIndex = -1;
 $("#undo").on('click', function () {
     //only execute function if there is a layer to be removed
     if (Math.abs(undoIndex) <= $('#canvas-real').getLayers().length) {
-        //only hiding the layer for now, not removing yet, incase of redo
-        /*if condition is a special statement for removing pencil, removing layers within the same
-        pencil(each pencil drawing has the same group, get .group[0] gets the group name, then setting
-        the property for the pencil group with the same name to invisible*/
-        if ($('#canvas-real').getLayer(undoIndex).groups[0]) {
-            $('#canvas-real').animateLayerGroup($('#canvas-real').getLayer(undoIndex).groups[0], {
-                visible: false
-            }, 0);
-            //specifically, for pencil, undoIndex moves until it exits the end of the group of pencil layer
-            const group = $('#canvas-real').getLayer(undoIndex).groups[0]
-            //typeof check is needed otherwise TypeError would be thrown, doesn't affect user experience, but bug should be avoided
-            while (typeof $('#canvas-real').getLayer(undoIndex) !== 'undefined' && $('#canvas-real').getLayer(undoIndex).groups[0] == group) {
-                undoIndex--;
-            }
-        } else {
             //simple removing layers for basic brushes
             $('#canvas-real').animateLayer(undoIndex, {
                 visible: false
             }, 0);
             //undoIndex moves to next item
             undoIndex--;
-        }
     }
     //disable the undo button if there is nothing more to be undone
     if (Math.abs(undoIndex) > $('#canvas-real').getLayers().length) {
-        $('#undo').prop('disabled', true); 
+        $('#undo').prop('disabled', true).css({'color':'grey'});
     }
     //enable the redo button
     if (undoIndex < -1) {
-        $('#redo').prop('disabled', false); 
+        $('#redo').prop('disabled', false).css({'color':'white'});
     }
 })
 
@@ -42,19 +26,6 @@ $("#redo").on('click', function () {
     if (undoIndex < -1) {
         //shift back to the previous hidden layer
         undoIndex++;
-        //if true, pencil, else, normal
-        if ($('#canvas-real').getLayer(undoIndex).groups[0]) {
-            //reset pencil group to be visible
-            $('#canvas-real').animateLayerGroup($('#canvas-real').getLayer(undoIndex).groups[0], {
-                visible: true
-            }, 0);
-            //undoIndex moves to previous item
-            const group = $('#canvas-real').getLayer(undoIndex).groups[0]
-            while ($('#canvas-real').getLayer(undoIndex).groups[0] == group) {
-                undoIndex++;
-            }
-            undoIndex--;
-        } else {
             //undoIndex moves to previous item
             $('#canvas-real').animateLayer(undoIndex, {
                 visible: true
@@ -62,19 +33,18 @@ $("#redo").on('click', function () {
         }
         //disable the redo button if there is nothing more to be redone
         if (undoIndex == -1) {
-            $('#redo').prop('disabled', true); 
+            $('#redo').prop('disabled', true).css({'color':'grey'});
         }
         //enable the undo button
         if (Math.abs(undoIndex) <= $('#canvas-real').getLayers().length) {
-            $('#undo').prop('disabled', false); 
+            $('#undo').prop('disabled', false).css({'color':'white'});
         }
-    }
 })
 
 //call this function when a new item is drawn, removing all the undid layers
 const resetUndo = () => {
-    $('#undo').prop('disabled', false);
-    $('#redo').prop('disabled', true);
+    $('#undo').prop('disabled', false).css({'color':'white'});
+    $('#redo').prop('disabled', true).css({'color':'grey'});
     while (undoIndex < -1) {
         $('#canvas-real').removeLayer(-1);
         undoIndex++;
